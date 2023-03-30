e4b2c5d6f5e46e122cf32dd6c2fb745a497231f6
doom <phramov945@gmail.com>
2023-03-30 15:17:51 +0300

    Add admin-panel action for load orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
-from .exchange_rate_service import USD_RATE
 from .models import Order
+from .tasks import get_orders
+from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME
+
+
+@admin.action(description='Load data from spreadsheet')
+def load_orders(modeladmin, request, queryset):
+    get_orders.delay()
 
 
 class OrderAdmin(admin.ModelAdmin):
     """
     Displays all fields in admin panel except 'id'.
     Allows only get data.
+    Has action to load data from spreadsheet
     """
-    readonly_fields = ('seq_num', 'order', 'price_usd', 'price_rub', 'required_date',)
+
     list_display = ('seq_num', 'order', 'price_usd', 'price_rub', 'required_date',)
     list_display_links = ('order',)
     search_fields = ('order',)
     ordering = ('seq_num',)
-
-    def price_rub(self, obj: Order):
-        if obj.price_usd and USD_RATE:
-            return int(obj.price_usd * USD_RATE)
-        else:
-            return None
-
-    price_rub.short_description = 'стоимость в руб.'
+    actions = (load_orders,)
 
     def has_add_permission(self, request):
         return False
@@ -30,6 +30,17 @@ class OrderAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    def changelist_view(self, request, extra_context=None):
+        """Allows call admin panel actions without selecting objects"""
+
+        if 'action' in request.POST and request.POST['action'] == 'load_orders':
+            if not request.POST.getlist(ACTION_CHECKBOX_NAME):
+                post = request.POST.copy()
+                for u in Order.objects.all():
+                    post.update({ACTION_CHECKBOX_NAME: str(u.id)})
+                request._set_post(post)
+        return super(OrderAdmin, self).changelist_view(request, extra_context)
 
 
 admin.site.register(Order, OrderAdmin)
