109c2759560cfe914a72a24e0485b84f50a164bd
doom <phramov945@gmail.com>
2023-03-30 15:10:40 +0300

    Add logic for <price_rub> column calculation

--- orders/management/commands/get_orders.py
+++ orders/management/commands/get_orders.py
@@ -2,7 +2,10 @@ import environ
 import gspread
 from django.core.management import BaseCommand
 import sqlalchemy as sa
-from .load_spreadsheet import gsheet2df
+from orders.load_spreadsheet import gsheet2df
+from orders.exchange_rate_service import CurrencyCBRRates
+import pandas as pd
+import numpy as np
 
 env = environ.Env()
 
@@ -23,6 +26,16 @@ class Command(BaseCommand):
 
             # Get Pandas DataFrame from spreadsheet
             df = gsheet2df(self.spreadsheet_name, 0)
+
+            # Calculate and update the 'price_rub' column according the actual USD rate
+            current_rates = CurrencyCBRRates()
+            usd_rate = current_rates.get_rate_by_code('USD')
+            df.loc[df['price_usd'].notna(), 'price_rub'] = df['price_usd'] * usd_rate
+
+            # Pass column 'price rub' to <Int64> safely with <NaN> values handling
+            df['price_rub'] = np.floor(pd.to_numeric(df['price_rub'], errors='coerce')).astype('Int64')
+
+            # Connect to SQL and save DataFrame to Order table with replace all values
             connection_string = env('CONNECTION_STR')
             engine = sa.create_engine(connection_string)
             engine.connect()
