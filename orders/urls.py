8963c44e270c46e1f8734dbd21cf65b61fc3dd0b
doom <phramov945@gmail.com>
2023-04-07 21:29:22 +0300

    Bugfix v1.4

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from .views import OrderViewSet
 
 router = routers.SimpleRouter()
-router.register('order', OrderViewSet)
+router.register('orders', OrderViewSet)
 
 urlpatterns = []
 urlpatterns += router.urls
